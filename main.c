f060325075bc8208ab7ee100174c381e04a874b7
root <tulkiidra@gmail.com>
2022-11-30 12:42:20 -0800

    adding fork command

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 /**
  * main - open a new bash
  * Return: 0
@@ -8,6 +10,7 @@
 
 int main(void)
 {
+	pid_t pid;
 	char *bf;
 	size_t bufsize = 1;
 	size_t characters;
@@ -18,14 +21,31 @@ int main(void)
 		perror("Unable to allocate buffer");
 		exit(1);
 	}
-	while (!feof(stdin))
+
+	pid = fork();
+
+	if (pid == -1) /* error */
+		return (0);
+
+	if (pid == 0) /* if child */
 	{
-		printf("($) ");
-		characters = getline(&bf, &bufsize, stdin);
-		if (!(bf[0] == 'e' && bf[1] == 'x' && bf[2] == 'i' && bf[3] == 't'))
-			printf("%s\n", bf);
-		else
-			exit(0);
+		while (!feof(stdin))
+		{
+			printf("($) ");
+			characters = getline(&bf, &bufsize, stdin);
+			if (!(bf[0] == 'e' && bf[1] == 'x' && bf[2] == 'i' && bf[3] == 't'))
+				printf("%s\n", bf);
+			else
+			{
+				free(bf);
+				exit(0);
+			}
+		}
 	}
+	else /* if parent */
+	{
+		wait(NULL); /* wait for the child to end */
+	}
+	free(bf);
 	return (0);
 }
