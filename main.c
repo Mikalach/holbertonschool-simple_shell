db0a5f4d86a81df1b8211e58a861408d137522e0
root <tulkiidra@gmail.com>
2022-12-02 08:58:02 -0800

    Adding exit everywhere i could, and probably pushing it in the wronk branch

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ char **_strtok1(char *line)
 	int i = 0;
 
 	av = NULL;
-	av = malloc(sizeof(char *) * 100);
-	tok = strtok(line,delim);
+	av = malloc(sizeof(char *) * 1024);
+	tok = strtok(line, delim);
 
 	while (tok)
 	{
@@ -20,40 +20,40 @@ char **_strtok1(char *line)
 		tok = strtok(NULL, delim);
 		i++;
 	}
+	av[i] == NULL;
 	return (av);
 }
 
-void frk(char **av, char **argenv)
+void frk(char **av, char **envp)
 {
 	pid_t pid;
+
 	pid = fork();
 	int status;
 
 	if (pid == -1) /* error */
-		return;
+		exit(1);
 	if (pid == 0) /* if child */
-		execve(av[0], av, argenv);
+	{
+		execve(av[0], av, envp);
+		exit(1);
+	}
 	else
-		wait(&status); /* wait for the child to end */
+		wait(NULL); /* wait for the child to end */
 }
 
-int main(void)
+int main(__attribute__ ((unused))int argc, __attribute__ ((unused))char **argv, char **envp)
 {
 	pid_t pid;
 	char *bf;
-	size_t bufsize = 10;
+	size_t bufsize = 1024;
 	int i = 0;
 	char **av;
 	char *tok;
-	char delim[] = " ";
-	char *argenv[] = {"Home=/root",
-		"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
-		"TZ=America/Los_Angeles",
-		"_=/usr/bin/env",
-		NULL};
 
 	av = NULL;
-	av = malloc(sizeof(char *) * 100);
+	av = malloc(sizeof(char *) * bufsize);
+	/* malloc error handle */
 
 	bf = (char *)malloc(bufsize * sizeof(char));
 	if (bf == NULL)
@@ -65,14 +65,16 @@ int main(void)
 	{
 		printf("($) ");
 		getline(&bf, &bufsize, stdin);
-		av = _strtok1(bf);
 		if (feof(stdin) || strcmp(bf, "exit\n") == 0)
 		{
 			printf("exit\n");
 			exit(0);
 		}
 		else
-			frk(av, argenv);
+		{
+			av = _strtok1(bf);
+			frk(av, envp);
+		}
 	}
 	free(bf);
 	return (0);
