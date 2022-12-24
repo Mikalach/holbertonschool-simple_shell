b0e02fc0f1d134d2d7af0e33001eb7d2c2131a66
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2022-12-24 02:49:50 +0100

    Get comments back

--- main.c
+++ main.c
@@ -77,8 +77,8 @@ int frk(char **av, char **envp, char *filename)
 	{
 		if (av && *av)
 		{
-			test = execve(av[0], av, envp);
-			if (test == -1)
+			test = execve(av[0], av, envp); /* run it */
+			if (test == -1) /* if run fail: error + exit*/
 			{
 				printf("%s: %s: command not found\n", filename, av[0]);
 				exit(EXIT_FAILURE);
@@ -86,7 +86,7 @@ int frk(char **av, char **envp, char *filename)
 		}
 		exit(EXIT_FAILURE);
 	}
-	else
+	else /* if parent */
 		wait(&status); /* wait for the child to end */
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
@@ -102,39 +102,56 @@ int frk(char **av, char **envp, char *filename)
  * Return: 0
  */
 int main(__attribute__((unused))int argc, char **argv, char **envp)
-{char *bf = NULL, **av = NULL, *pathBuffer = NULL;
+{
+	char *bf = NULL**av = NULL, *pathBuffer = NULL;
 	size_t bufsize = 1024;
 	int ext = 0, path = 100, freeAvTest, isOnlySpaces, wifeexit = 0;
 	struct stat st;
 
+
+	/* Array-Buffer creation */
 	av = calloc(1024, sizeof(char *));
 	if (av == NULL)
-		ERR(av);
+		ERR(av); /* free + print stderr + exit */
 	bf = calloc(1024, sizeof(char));
 	if (bf == NULL)
-		ERR(bf);
+		ERR(bf); /* free + print stderr + exit */
 	pathBuffer = calloc(1000, sizeof(char));
 	if (pathBuffer == NULL)
-		ERR(pathBuffer);
+		ERR(pathBuffer); /* free + print stderr + exit */
+
+
+	/* get the path of built-in function using environment */
 	path = getPath(envp);
+
+
+	/* The Simple Shell */
 	while (1)
-	{	SETVAR;
-		if (path != 100)
+	{
+		SETVAR; /* set all necessary test variables to 0 */
+
+		if (path != 100) /* if environment/built-in path is found*/
 			strcpy(pathBuffer, envp[path]);
-		getline(&bf, &bufsize, stdin);
-		if (feof(stdin) || strcmp(bf, "exit\n") == 0)
+
+		getline(&bf, &bufsize, stdin); /* get user input */
+
+		if (feof(stdin) || strcmp(bf, "exit\n") == 0) /* launch 'exit' command to stop the program */
 			ext = 1;
-		else if (strcmp(bf, "env\n") == 0)
+
+		else if (strcmp(bf, "env\n") == 0) /* launch 'env' command */
 			printfullenv(envp);
-		else
-		{	isOnlySpaces = _strtok1(av, bf);
-			if (isOnlySpaces == 0)
+
+		else /* launch any built-in command if existing */
+		{
+			isOnlySpaces = _strtok1(av, bf); /* get the user input + handle the special case : no character other than ' ', '\n' or '\t' input */
+			if (isOnlySpaces == 0) /* if user input isn't '\0', '\n', '\t', ' ' */
 			{
-				freeAvTest = _path1(pathBuffer, &av[0]);
+				freeAvTest = _path1(pathBuffer, &av[0]); /* get the absolute path of command to run it */
 				if (stat(av[0], &st) == 0)
 				{
+					/* run the command if existing */
 					wifeexit = frk(av, envp, argv[0]);
-					if (wifeexit != 0)
+					if (wifeexit != 0) /* if error encountered, free + exit */
 					{
 						FREEALL;
 						exit(wifeexit);
@@ -144,10 +161,15 @@ int main(__attribute__((unused))int argc, char **argv, char **envp)
 					dprintf(STDERR_FILENO, "./hsh: 1: %s: not found\n", av[0]);
 			}
 		}
+		
+		/* stop the program loop */
 		if (ext == 1)
 			break;
 	if (freeAvTest == 1)
-		free(av[0]); }
-	FREEALL;
+		free(av[0]); 
+	}
+	/* end of the shell programm (and end of the loop in the prog)*/
+
+	FREEALL; /* free every buffer */
 	return (0);
 }
