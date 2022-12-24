b69ec96cfbe9e8716c7d4de089c28e415f0b7fd9
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2022-12-24 02:53:30 +0100

    Update simples.h

--- simples.h
+++ simples.h
@@ -12,6 +12,11 @@
 #define ERR(s) {perror("Allocation failed ((s))"); exit((1));}
 #define SETVAR {freeAvTest = 0; isOnlySpaces = 0;}
 #define FREEALL {free(pathBuffer); free(bf); free(av);}
+
 int _path1(char *impath, char **usrinpt);
 int only_space(char *usrinpt);
+void printfullenv(char **envp);
+int getPath(char **envp);
+int _strtok1(char **av, char *line);
+int frk(char **av, char **envp, char *filename);
 #endif
